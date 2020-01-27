b18ec11e72c5aa553f8c71b683eac35967c3521e
Tenacious-Qi <joe.mccann@mailbox.org>
2020-01-26 19:23:54 -0500

    changed filenames

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 let playerScore = 0;
 let computerScore = 0;
+let playerSelection, choice, result, win, score, choicePlayer, choiceCPU, playAgain, scorePlayerDisplay, scoreCpuDisplay, rules, gif, gifLoss;
 
-    // determines computer's choice
+choicePlayer = document.querySelector(".choice-player");
+choiceCPU = document.querySelector(".choice-cpu");
+result = document.querySelector(".result");
+win = document.querySelector(".win");
+playAgain = document.querySelector(".play-again");
+gif = document.querySelector(".gif");
+gifLoss = document.querySelector(".gif-loss");
+scorePlayerDisplay = document.querySelector(".score-player-display");
+scoreCpuDisplay = document.querySelector(".score-cpu-display");
+rules = document.querySelector(".rules");
+
+gif.style.display = "none";
+gifLoss.style.display = "none";
+playAgain.style.display = "none";
+rules.style.display = "none";
 
 function computerPlay() {
     
@@ -18,79 +33,105 @@ function computerPlay() {
    }
 }
 
-    // function to define player's choice
+    rock = document.querySelector('#rock');
+    rock.addEventListener('click', () => {
+    playerSelection = "rock";
+    });
 
-function promptHuman () {
-    return window.prompt("Rock, paper, or scissors", "rock").toLowerCase();
+    paper = document.querySelector('#paper');
+    paper.addEventListener('click', () => {
+    playerSelection = "paper";
+    });
+
+    scissors = document.querySelector('#scissors');
+    scissors.addEventListener('click', () => {
+    playerSelection = "scissors";
+    });
+
+    const icons = document.querySelectorAll("img.icon");
+    let transformText = () => {result.classList.toggle("active");}
+
+    icons.forEach((icon) => {
+        icon.addEventListener("click", (e) => {
+            gameLogic();
+            playRound();
+            transformText();
+    });
+});
+
+function gameLogic() {
+    if (((playerScore === 5) && (playerScore > computerScore)) || ((computerScore === 5) && (computerScore > playerScore))) {
+        newGame();
+    } 
 }
-
-    //plays a round of Rock Paper Scissors and displays results
 
 function playRound() {
     
-    const playerSelection = promptHuman();
-    const computerSelection = computerPlay()
-
-    if (playerSelection == computerSelection) {
-            console.log("Player choice: " + playerSelection + " | " + "CPU choice: " + computerSelection);
-            console.log("Player: " + playerScore + " | " + "CPU: " + computerScore);
-            return ("It's a draw");
-
-    }   else if (playerSelection == "rock" && computerSelection == "paper") {
-            computerScore++
-            console.log("Player choice: " + playerSelection + " | " + "CPU choice: " + computerSelection);
-            console.log("Player: " + playerScore + " | " + "CPU: " + computerScore);
-            return ("You lose! Paper covers Rock.");
-
-    }   else if (playerSelection == "paper" && computerSelection == "rock") {
-            playerScore++
-            console.log("Player choice: " + playerSelection + " | " + "CPU choice: " + computerSelection);
-            console.log("Player: " + playerScore + " | " + "CPU: " + computerScore);
-            return ("You win! Paper covers Rock.");
-            
-
-    }   else if (playerSelection == "paper" && computerSelection == "scissors") {
-            computerScore++
-            console.log("Player choice: " + playerSelection + " | " + "CPU choice: " + computerSelection);
-            console.log("Player: " + playerScore + " | " + "CPU: " + computerScore);
-            return("You lose! Scissors cut paper."); 
-
-    }   else if (playerSelection == "scissors" && computerSelection == "paper") {
-            playerScore++
-            console.log("Player choice: " + playerSelection + " | " + "CPU choice: " + computerSelection);
-            console.log("Player: " + playerScore + " | " + "CPU: " + computerScore);
-            return("You win! Scissors cut paper.");
-
-    }   else if (playerSelection == "scissors" && computerSelection == "rock") {
-            computerScore++
-            console.log("Player choice: " + playerSelection + " | " + "CPU choice: " + computerSelection);
-            console.log("Player: " + playerScore + " | " + "CPU: " + computerScore);
-            return("You lose! Rock beats scissors.");
-
-    }   else if (playerSelection == "rock" && computerSelection == "scissors") {
-            playerScore++
-            console.log("Player choice: " + playerSelection + " | " + "CPU choice: " + computerSelection);
-            console.log("Player: " + playerScore + " | " + "CPU: " + computerScore);
-            return("You win! Rock beats scissors.");
-
-    }   else {
-        return ("Please enter rock, paper, or scissors");
-    }
+    const computerSelection = computerPlay();
+    result.style.display = "block";
+    rules.style.display = "block";
     
+        if (playerSelection === computerSelection) {
+        result.textContent = "It's a draw.";
+        
+    }   else if (playerSelection === "rock" && computerSelection === "paper") {
+            computerScore++
+            result.textContent = "You lose the round, paper covers rock.";
+            
+    }   else if (playerSelection === "paper" && computerSelection === "rock") {
+            playerScore++
+            result.textContent = "You win the round, paper covers rock.";
+
+    }   else if (playerSelection === "paper" && computerSelection === "scissors") {
+            computerScore++
+            result.textContent = "You lose the round, scissors cut paper.";
+
+    }   else if (playerSelection === "scissors" && computerSelection === "paper") {
+            playerScore++
+            result.textContent = "You win the round, scissors cut paper.";
+
+    }   else if (playerSelection === "scissors" && computerSelection === "rock") {
+            computerScore++
+            result.textContent = "You lose the round, rock beats scissors.";
+
+    }   else if (playerSelection === "rock" && computerSelection === "scissors") {
+            playerScore++
+            result.textContent = "You win the round, rock beats scissors.";
+    } 
+
+    choicePlayer.textContent = "player: " + playerSelection;
+    choiceCPU.textContent = "cpu: " + computerSelection;
+    scorePlayerDisplay.textContent = "player: " + playerScore;
+    scoreCpuDisplay.textContent = "cpu: " + computerScore;
+
+    if (playerScore === 5) {
+            winDisplay();
+            gif.style.display = "block";
+            win.textContent = "Congratulations, you won the game!";
+
+    }   else if (computerScore === 5) {
+            winDisplay();
+            gifLoss.style.display = "block";
+            win.textContent = "Sorry, you lost the game.";
+    }
 }
 
-for(i = 0; i < 5; i ++) {
-    console.log("Round " + (i + 1) + " result: " + playRound());
+function winDisplay() {
+    win.style.display = "block";
+    playAgain.style.display = "block";
+    result.style.display = "none";
 }
 
-console.log("Final Score, Player: " + playerScore + " | " + "CPU: " + computerScore);
+function newGame() {
+    playerScore = 0;
+    computerScore = 0;
+    scorePlayerDisplay.textContent = "player: " + playerScore;
+    scoreCpuDisplay.textContent = "cpu: " + computerScore;
+    result.style.display = "none";
+    result.style.marginBottom = "20px";
+    win.style.display = "none";
+    gif.style.display = "none";
+    gifLoss.style.display = "none";
+    playAgain.style.display = "none";
+}
 
-if (playerScore === computerScore) {
-    console.log("It's a draw")
-}
-else if (playerScore > computerScore) {
-    console.log("You win!")
-}
-else {
-    console.log("Sorry, you lose.")
-}
